5464e28e29ae7d4ca4298071113292e4290a70e1
Xavier Clerc <xclerc@janestreet.com>
2019-08-01 09:37:28 +0100

    v0.13-preview.120.47+139

--- bench/src/import.ml
+++ bench/src/import.ml
@@ -2,7 +2,7 @@ module Incr = Incremental.Make ()
 module Incr_map = Incr_map.Make (Incr)
 module Var = Incr.Var
 module Obs = Incr.Observer
-module Bench = Core_bench.Std.Bench
+module Bench = Core_bench.Bench
 include Incr.Let_syntax
 
 module Infix = struct
